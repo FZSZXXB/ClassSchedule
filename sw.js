2664639a501881600cab5c47a645cd6dd51f5aa7
Hao_cen <haocen.minecraft@gmail.com>
2021-05-24 16:59:01 +0800

    update pwa

--- sw.js
+++ sw.js
@@ -1,57 +1,48 @@
-var cacheStorageKey = 'classSchedule-v1'
+var cacheStorageKey = 'classSchedule-v2.0'
 
 var cacheList = [
   '/',
   "index.html",
   "/assets/css/index.css",
   "/assets/imgs/loading.svg",
-  "/assets/imgs/icon.png",
+  // "/assets/imgs/icon.png",
   "/assets/scripts/data.js",
   "/assets/scripts/main.js",
   "/assets/scripts/Timetable.js"
 ]
 
-self.addEventListener('install', function(e) {
-    console.log('Cache event!')
+  self.addEventListener('install', function(e) {
+    console.log('[Service Worker] Install');
     e.waitUntil(
       caches.open(cacheStorageKey).then(function(cache) {
-        console.log('Adding to Cache:', cacheList)
-        return cache.addAll(cacheList)
-      }).then(function() {
-        console.log('Skip waiting!')
-        return self.skipWaiting()
+        console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(cacheList);
       })
-    )
-  })
-  
+    );
+  });
   self.addEventListener('activate', function(e) {
-    console.log('Activate event')
     e.waitUntil(
-      Promise.all(
-        caches.keys().then(cacheNames => {
-          return cacheNames.map(name => {
-            if (name !== cacheStorageKey) {
-              return caches.delete(name)
-            }
-          })
-        })
-      ).then(() => {
-        console.log('Clients claims.')
-        return self.clients.claim()
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if(cacheStorageKey.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        }));
       })
-    )
-  })
-  
+    );
+  });
+
   self.addEventListener('fetch', function(e) {
-    // console.log('Fetch event:', e.request.url)
     e.respondWith(
-      caches.match(e.request).then(function(response) {
-        if (response != null) {
-          console.log('Using cache for:', e.request.url)
-          return response
-        }
-        console.log('Fallback to fetch:', e.request.url)
-        return fetch(e.request.url)
+      caches.match(e.request).then(function(r) {
+        console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then(function(response) {
+          return caches.open(cacheStorageKey).then(function(cache) {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
       })
-    )
-  })
+    );
+  });
